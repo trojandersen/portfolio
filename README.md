9231bf0db52bb7e6ccd06e2dc4d0766658cb85ca
Troj Andersen <trojandersen@gmail.com>
2025-03-12 12:44:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio Troj Andersen
+
+Examproject for my FE23 class at Folkuniversitetet.
+
+Made with Vite, React, TailwindCSS & React3Fiber.
+
+Work in Progress..
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex justify-center text-blue-500">
+      <h1>Work in Progress</h1>
+    </main>
+  );
+}
+
+export default App;
