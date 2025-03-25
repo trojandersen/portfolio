6ab0434c706d904e4624b0f5a3f6b3627f39b917
Troj Andersen <trojandersen@gmail.com>
2025-03-25 12:58:37 +0100

    background and comments added

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,27 @@ import { OrbitControls, Center } from "@react-three/drei";
 import { Suspense } from "react";
 import CanvasLoader from "./components/CanvasLoader";
 import OldTvModel from "./components/OldTvModel";
-
+import SkyDome from "./components/SkyDome";
 function App() {
   return (
     <main className="h-screen flex items-center justify-center">
+      {/* The canvas is used to display the 3d environment within our React Project */}
       <Canvas className="w-full h-full">
+        {/* The Suspense component is used as a fallback, which shows before the Canvas has been loaded in */}
         <Suspense fallback={<CanvasLoader />}>
+          {/* The OrbitControls component is used to allow the user to move around the 3d environment. maxPolarAngle disallows the camera to show under the tv. Will be used later on. */}
           <ambientLight intensity={0.5} />
-          <directionalLight color="white" position={[5, 5, 25]} />
+          <directionalLight color="white" position={[5, 5, 15]} />
           <OrbitControls
-            minDistance={500}
+            minDistance={50}
             maxPolarAngle={Math.PI / 2}
-            enableZoom={false}
-            enablePan={false}
+            enableZoom={true}
+            enablePan={true}
           />
+          {/* Here the Skydome & the TV model is imported and scaled using the useGLTF hook to show the .gltf models. Center centers the TV in the 3d space. */}
+          <SkyDome scale={800} />
           <Center>
-            <OldTvModel scale={0.5} />
+            <OldTvModel scale={0.05} />
           </Center>
         </Suspense>
       </Canvas>
